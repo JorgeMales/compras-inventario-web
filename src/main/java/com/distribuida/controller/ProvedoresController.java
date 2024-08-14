ff563085e6899c6b3452f9df2bdf29ce32dfd5d8
MATUTINO <MATUTINO@DESKTOP-5HH3LJH>
2024-08-14 08:49:48 -0500

    JQv0002 - Uso de try catch en ProvedoresController

--- src/main/java/com/distribuida/controller/ProvedoresController.java
+++ src/main/java/com/distribuida/controller/ProvedoresController.java
@@ -17,70 +17,66 @@ import com.distribuida.entities.Provedores;
 @Controller
 @RequestMapping("/provedores")
 public class ProvedoresController {
-	
-	@Autowired
-	private ProvedoresDAO provedoresDAO;
-	
-	@GetMapping("/findAll")
-	
-	public String findAll(Model model) {
-		
-		List <Provedores> provedores= provedoresDAO.findAll();
-		model.addAttribute("provedores",provedores);
-		
-		return "listar-provedores";
-		
-	}
-	
-	@GetMapping("/findOne")
-	public String findOne(@RequestParam("idProvedores")@Nullable Integer idProvedores
-			             ,@RequestParam("opcion")@Nullable Integer opcion
-			             ,Model model
-			
-			
-			) {
-		    if (idProvedores !=null) {
-		    	Provedores provedores = provedoresDAO.findOne(idProvedores);
-		    	model.addAttribute("provedores",provedores);
-		    	
-				
-			}
-		    if (opcion == 1) return "add-provedores";
-		    else return "del-provedores";
-		
-	}
-	
-	@PostMapping("/add")
-	public String add(@RequestParam("idProvedores") @Nullable Integer idProvedores
-			         ,@RequestParam("nombre") @Nullable String nombre
-		             ,@RequestParam("direccion") @Nullable String direccion
-		             ,@RequestParam("telefono") @Nullable String telefono
-		             ,@RequestParam("email") @Nullable String email
-		             ,Model model
-			
-			){
-		
-		if (idProvedores == null) {
-			Provedores provedores = new Provedores(0, nombre, direccion, telefono, email);
-			provedoresDAO.add(provedores);
-			
-			
-		} else {
-			Provedores provedores = new Provedores(idProvedores,nombre, direccion, telefono, email);
-			provedoresDAO.up(provedores);
 
-		}
-		return "redirect:/provedores/findAll";
-	}
-	 
-	@GetMapping("/del")
-	public String del(@RequestParam("idProvedores") @Nullable Integer idProvedores) {
-		
-		
-		provedoresDAO.del(idProvedores);
-		
-		return"redirect:/provedores/findAll";
-	}
-	
+    @Autowired
+    private ProvedoresDAO provedoresDAO;
 
+    @GetMapping("/findAll")
+    public String findAll(Model model) {
+        // try {
+            List<Provedores> provedores = provedoresDAO.findAll();
+            model.addAttribute("provedores", provedores);
+            return "listar-provedores";
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
+    }
+
+    @GetMapping("/findOne")
+    public String findOne(@RequestParam("idProvedores") @Nullable Integer idProvedores,
+                          @RequestParam("opcion") @Nullable Integer opcion,
+                          Model model) {
+        // try {
+            if (idProvedores != null) {
+                Provedores provedores = provedoresDAO.findOne(idProvedores);
+                model.addAttribute("provedores", provedores);
+            }
+            if (opcion == 1) return "add-provedores";
+            else return "del-provedores";
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
+    }
+
+    @PostMapping("/add")
+    public String add(@RequestParam("idProvedores") @Nullable Integer idProvedores,
+                      @RequestParam("nombre") @Nullable String nombre,
+                      @RequestParam("direccion") @Nullable String direccion,
+                      @RequestParam("telefono") @Nullable String telefono,
+                      @RequestParam("email") @Nullable String email,
+                      Model model) {
+        // try {
+            if (idProvedores == null) {
+                Provedores provedores = new Provedores(0, nombre, direccion, telefono, email);
+                provedoresDAO.add(provedores);
+            } else {
+                Provedores provedores = new Provedores(idProvedores, nombre, direccion, telefono, email);
+                provedoresDAO.up(provedores);
+            }
+            return "redirect:/provedores/findAll";
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
+    }
+
+    @GetMapping("/del")
+    public String del(@RequestParam("idProvedores") @Nullable Integer idProvedores) {
+        // try {
+            provedoresDAO.del(idProvedores);
+            return "redirect:/provedores/findAll";
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
+    }
 }
+
